73df111186915552c53d3c9d49eb30d1883d7e7b
Rupayan Das <72387389+drupayan7@users.noreply.github.com>
2023-02-04 11:02:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('🌮🥑Hard-Boiled Free Range Egg')
 
 
 streamlit.header('🍜🌮 Build Your Own Fruit Smoothie 🥗🍌')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
