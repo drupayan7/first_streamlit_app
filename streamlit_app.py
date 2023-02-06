d8b817072bfea2e1143c542977c74b215e70a51d
Rupayan Das <72387389+drupayan7@users.noreply.github.com>
2023-02-06 15:47:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,17 +22,24 @@ streamlit.text('🌮🥑Hard-Boiled Free Range Egg')
 streamlit.header('🍜🌮 Build Your Own Fruit Smoothie 🥗🍌')
 
 
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
 def set_background(png_file):
     bin_str = get_base64(png_file)
     page_bg_img = '''
     <style>
-    .stApp {
-    background-image: url("data:https://images.unsplash.com/photo-1542281286-9e0a16bb7366.png;base64,%s");
+    body {
+    background-image: url("data:image/png;base64,%s");
     background-size: cover;
     }
     </style>
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./src/static/img/background.png')
 
 
 #import pandas
